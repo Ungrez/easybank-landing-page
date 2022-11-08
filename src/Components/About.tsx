6cf702ba6b65a2b0a8afb805214c588e7be1225f
Piotr Machera <piotr.mache@gmail.com>
2022-11-08 16:00:00 +0100

    Articles, About mobile look

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,5 +1,32 @@
 import "../sass/ComponentsStyles/About.scss";
+
 const About = () => {
+  const items: {
+    title: string;
+    about: string;
+  }[] = [
+    {
+      title: "Online Banking",
+      about:
+        "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+    },
+    {
+      title: "Simple Budgeting",
+      about:
+        "See exactly where your money goes each month. Receive notifications when you're close to your hitting limits.",
+    },
+    {
+      title: "Fast Onboarding",
+      about:
+        "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+    },
+    {
+      title: "Open API",
+      about:
+        "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+    },
+  ];
+
   return (
     <div id="about">
       <div id="about_description">
@@ -9,14 +36,13 @@ const About = () => {
           hub. Control your finances like never before.
         </p>
       </div>
-      <div id="about_box">
-        <div id="about_box_img"></div>
-        <p>Online Banking</p>
-        <p>
-          Our modern web and mobile applications allow you to keep track of your
-          finances wherever you are in the world.
-        </p>
-      </div>
+      {items.map((item, index) => (
+        <div key={index} id="about_box">
+          <div id="about_box_img"></div>
+          <p>{item.title}</p>
+          <p>{item.about}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Components/Articles.tsx
+++ src/Components/Articles.tsx
@@ -1,5 +1,46 @@
+import "../sass/ComponentsStyles/Articles.scss";
 const Articles = () => {
-  return <h1>Articles</h1>;
+  const items = [
+    {
+      author: "Claire Robinson",
+      title: "Receive money in any currency with no fees",
+      more: "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single ...",
+      path: require("../imgs/image-currency.jpg"),
+    },
+    {
+      author: "Wilson Hutton",
+      title: "Treat yourself without worrying about money",
+      more: "Out simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you ...",
+      path: require("../imgs/image-restaurant.jpg"),
+    },
+    {
+      author: "Wilson Hutton",
+      title: "Take your Easybank card wherever you go",
+      more: "We want you to enjoy travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you ...",
+      path: require("../imgs/image-plane.jpg"),
+    },
+    {
+      author: "Claire Robinson",
+      title: "Our invite-only Beta accounts are now live!",
+      more: "After a lot of hard work by whole team, we're excited to launch our closed beta. It's easy to request an invite throught the site ...",
+      path: require("../imgs/image-confetti.jpg"),
+    },
+  ];
+  return (
+    <div id="articles">
+      <p>Latest Articles</p>
+      <div id="articles_container">
+        {items.map((item, index) => (
+          <div key={index} id="articles_container_box">
+            <img src={item.path} alt={item.title} />
+            <p id="article_author">By {item.author}</p>
+            <p id="article_title">{item.title}</p>
+            <p id="article_desc">{item.more} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Articles;
