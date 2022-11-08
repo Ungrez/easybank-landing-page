ffea04559011877adf740231ded8b112d6107523
Piotr Machera <piotr.mache@gmail.com>
2022-11-08 13:45:11 +0100

    header mobile look

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,5 +1,24 @@
+import "../sass/ComponentsStyles/About.scss";
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div id="about">
+      <div id="about_description">
+        <p>Why choose Easybank?</p>
+        <p>
+          We leverage Open Banking to turn your bank account into your financial
+          hub. Control your finances like never before.
+        </p>
+      </div>
+      <div id="about_box">
+        <div id="about_box_img"></div>
+        <p>Online Banking</p>
+        <p>
+          Our modern web and mobile applications allow you to keep track of your
+          finances wherever you are in the world.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,22 @@
+import "../sass/ComponentsStyles/Header.scss";
+
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div id="header">
+      <div id="header_images">
+        <div id="header_images_phones"></div>
+      </div>
+      <div id="header_text">
+        <p>Next generation digital banking</p>
+        <p>
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button>Request Invite</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
