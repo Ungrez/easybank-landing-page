162e9cd2be127d6e1d8e1cd16ad02ce709b85e9b
Piotr Machera <piotr.mache@gmail.com>
2022-11-08 02:43:28 +0100

    initial commit

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,12 @@
+import "../sass/ComponentsStyles/Nav.scss";
+import logo from "../images/logo.svg";
+
+const Nav = () => {
+  return (
+    <nav id="nav">
+      <img id="nav_logo" src="../images/logo.svg" alt="logo" />
+    </nav>
+  );
+};
+
+export default Nav;
